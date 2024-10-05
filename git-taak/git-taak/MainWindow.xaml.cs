fb74c05d0469b7590eaffb85aae09edac2137f19
Golsoum <145329299+Golsoum@users.noreply.github.com>
2024-10-05 17:14:27 +0200

    KLEUR-03 Hover eFFECT

--- git-taak/git-taak/MainWindow.xaml.cs
+++ git-taak/git-taak/MainWindow.xaml.cs
@@ -20,5 +20,41 @@ namespace git_taak
         {
             InitializeComponent();
         }
+        private void yellowEvent(object sender, EventArgs e)
+        {
+            mainWindow.Background = Brushes.Yellow;
+        } 
+        private void leaveYellow(object sender, EventArgs e)
+        {
+            mainWindow.Background = Brushes.LightGray;
+        }
+       
+        private void greenEvent(object sender, EventArgs e)
+        {
+            mainWindow.Background = Brushes.Green; 
+        }
+        private void leaveGreen(object sender, EventArgs e)
+        {
+            mainWindow.Background = Brushes.LightGray;
+        }
+        private void blueEvent(object sender,EventArgs e)
+        {
+            mainWindow.Background = Brushes.Blue; 
+        }
+        private void leaveBlue(object sender, EventArgs e)
+        {
+            mainWindow.Background = Brushes.LightGray;
+        }
+        private void redEvent(object sender, EventArgs e)
+        {
+            mainWindow.Background = Brushes.Red; 
+        }
+        private void leaveRed(object sender, EventArgs e)
+        {
+            mainWindow.Background = Brushes.LightGray;
+        }
+
+
+
     }
 }
